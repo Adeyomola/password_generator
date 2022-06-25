3d10d3055f3d2f5e97c7c3b58745a6a81f92ad94
Adeyomola <kayzeel@yahoo.com>
2022-06-25 12:47:24 +0100

    Updated JS for Improved Character Inclusion in Passwords

--- password_generator.js
+++ password_generator.js
@@ -1,7 +1,9 @@
-const characters =
-  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()_+-=|{}[]:;?/><,.";
+const characters = "~`!@#$%^&*()_+-=|{}[]:;?/><,.";
+const numbers = "0123456789";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let store = "";
-let password;
+let password = "";
 let output = document.querySelector("#output");
 let button = document.querySelector("button");
 let clear = document.getElementById("clear");
@@ -14,16 +16,16 @@ function generatePassword() {
   if (passwordLength == "" || passwordLength == 0) {
     return;
   }
-  for (i = 0; i <= passwordLength; i++) {
-    let characterIndex = Math.floor(Math.random() * characters.length);
-    let char = characters[characterIndex];
-    store += char;
-    password = store;
-    if (password.length < passwordLength) {
-      continue;
-    }
+  for (i = 0; i < passwordLength; i++) {
+    let operations = [
+      characters[Math.floor(Math.random() * characters.length)],
+      numbers[Math.floor(Math.random() * numbers.length)],
+      lowerCase[Math.floor(Math.random() * lowerCase.length)],
+      upperCase[Math.floor(Math.random() * upperCase.length)],
+    ];
+    store += operations[Math.floor(Math.random() * operations.length)];
+    password = store.slice(store.length - passwordLength);
     output.value = password;
-    return (store = "");
   }
 }
 
